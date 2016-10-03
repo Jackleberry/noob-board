c5a3b919e8d21561def8422c1044211e731bd520
jackleberry <jackiehannaford@yahoo.co.uk>
2016-10-03 13:15:40 +0900

    Using connect for filter link

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,42 +1,27 @@
 import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
 import Link from './Link';
 
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { filter, children } = props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={filter === state.visibilityFilter}
-        onClick={() =>
-          store.dispatch({
-            type: "SET_VISIBILITY_FILTER",
-            filter
-          })
-        }>{children}</Link>
-    );
-  }
-}
-
-FilterLink.propTypes = {
-  filter: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter,
+    children: ownProps.children
+  };
 };
 
-FilterLink.contextTypes = {
-  store: PropTypes.object.isRequired
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () =>
+      dispatch({
+        type: "SET_VISIBILITY_FILTER",
+        filter: ownProps.filter
+      })
+  };
 };
+
+const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link);
 
 export default FilterLink;
